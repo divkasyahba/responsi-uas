bb0199646cbe7287e96d6bf6175b275cb077b4be
divkasyahba <divkasyahba@students.amikom.ac.id>
2021-07-13 15:51:41 +0700

    commit

---
+++ ProjectMahasiswa/Mahasiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+       
+
+        public string nim;
+        public string nama;
+        public string jeniskelamin;
+        public string ipk;
+
+        
+        public Mahasiswa(string nim, string nama, string jeniskelamin, string ipk)
+        {
+            this.nim = nim;
+            this.nama = nama;
+            this.jeniskelamin = jeniskelamin;
+            this.ipk = ipk;
+
+        }
+    }
+}
